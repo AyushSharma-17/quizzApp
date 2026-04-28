d95d313dcd46dae9392639817369f3e985284c08
ayush <ayushsharma12345qwerty@gmail.com>
2026-04-28 17:18:46 +0530

    #6 quiz service and dao created

--- src/main/java/com/ayush/quizzApp/Controller/QuizController.java
+++ src/main/java/com/ayush/quizzApp/Controller/QuizController.java
@@ -1,5 +1,8 @@
 package com.ayush.quizzApp.Controller;
 
+import com.ayush.quizzApp.Service.QuizService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,10 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/quiz")
 public class QuizController {
-        //http://localhost:8080/quiz/create?category=java&numQ=5&title=Jquiz
+       
+    @Autowired
+    QuizService quizService;
+
+    //http://localhost:8080/quiz/create?category=java&numQ=5&title=Jquiz
     @PostMapping("create")
     public ResponseEntity<String> createQuiz(@RequestParam String category , @RequestParam int numQ,@RequestParam String title) {
-        return new ResponseEntity<>("from the create quiz", HttpStatus.OK);
+        return quizService.createQuiz(category,numQ,title);
         
     
     }
---
+++ src/main/java/com/ayush/quizzApp/Service/QuizService.java
@@ -0,0 +1,37 @@
+package com.ayush.quizzApp.Service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.ayush.quizzApp.Dao.QuestionDao;
+import com.ayush.quizzApp.Dao.QuizDao;
+import com.ayush.quizzApp.model.Question;
+import com.ayush.quizzApp.model.Quiz;
+
+@Service
+public class QuizService {
+    @Autowired
+    QuizDao quizDao;
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public ResponseEntity<String> createQuiz(String category, int numQ, String title) {
+
+        List<Question> questions= questionDao.findRandomQuestionByCategory(category,numQ);
+        Quiz quiz = new Quiz();
+        quiz.setTitle(title);
+        quiz.setQuestions(questions); // plural setter
+
+quizDao.save(quiz);
+
+return ResponseEntity.status(HttpStatus.CREATED).body("Quiz created successfully");
+
+
+    }
+
+}
